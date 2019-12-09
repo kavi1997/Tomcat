b3be877793739229cda4ccaf3e0b027e171330e5
kavi1997 <37212467+kavi1997@users.noreply.github.com>
2019-12-09 15:44:57 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -4,19 +4,27 @@
 yum install java-1.8.0-openjdk-devel
 java -version
 
-#download the latest version of Apache Tomcat 9
-cd /opt
-wget http://mirrors.fibergrid.in/apache/tomcat/tomcat-8/v8.5.35/bin/apache-tomcat-8.5.35.tar.gz
-tar -xvzf /opt/apache-tomcat-8.5.35.tar.gz
+wget http://apachemirror.wuchna.com/tomcat/tomcat-8/v8.5.49/bin/apache-tomcat-8.5.49.tar.gz
 
-chmod +x /opt/apache-tomcat-8.5.35/bin/startup.sh shutdown.sh
+tar -xvzf apache-tomcat-8.5.49.tar.gz
 
-ln -s /opt/apache-tomcat-8.5.35/bin/startup.sh /usr/local/bin/tomcatup
-ln -s /opt/apache-tomcat-8.5.35/bin/shutdown.sh /usr/local/bin/tomcatdown
-tomcatup
+rm -rf apache-tomcat-8.5.49.tar.gz
+
+sed -i 's/port="8080"/port="8880"/' apache-tomcat-8.5.49/conf/server.xml
+
+sed -i 's\</tomcat-users>\<!-- -->\g' apache-tomcat-8.5.49/conf/tomcat-users.xml
+
+echo '<role rolename="manager-gui" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+echo '<user username="admin" password="admin" roles="manager-gui" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+echo '<role rolename="manager-script" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+echo '<user username="script" password="script" roles="manager-script" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+echo '</tomcat-users>' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
 
 
-#checkpoint
-cd /opt/apache-tomcat-8.5.35/conf
-# update port number in the "connecter port" field in server.xml
-# restart tomcat after configuration update
+
+sed -i 's/<Valve /<!-- <Valve /' apache-tomcat-8.5.49/webapps/manager/META-INF/context.xml
+
+sed -i 's\:1" />\:1" /> -->\g' apache-tomcat-8.5.49/webapps/manager/META-INF/context.xml
+
+
+apache-tomcat-8.5.49/bin/./startup.sh
