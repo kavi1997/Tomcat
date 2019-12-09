46c430f14fce34ac2abda6b9e59615d6447b4f28
kavi1997 <37212467+kavi1997@users.noreply.github.com>
2019-12-09 15:32:50 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -5,15 +5,18 @@ yum install java-1.8.0-openjdk-devel
 java -version
 
 #download the latest version of Apache Tomcat 9
-cd /usr/local
-wget http://apachemirror.wuchna.com/tomcat/tomcat-9/v9.0.26/bin/apache-tomcat-9.0.26.tar.gz
-tar -xvf apache-tomcat-9.0.26.tar.gz
-mv apache-tomcat-9.0.26 tomcat9
+cd /opt
+wget http://mirrors.fibergrid.in/apache/tomcat/tomcat-8/v8.5.35/bin/apache-tomcat-8.5.35.tar.gz
+tar -xvzf /opt/apache-tomcat-8.5.35.tar.gz
 
-#configure a CATALINA_HOME environment variable
-echo "export CATALINA_HOME="/usr/local/tomcat9"" >> ~/.bashrc
-source ~/.bashrc
+chmod +x /opt/apache-tomcat-8.5.35/bin/startup.sh shutdown.sh
 
-#start the tomcat web server
-cd /usr/local/tomcat9/bin
-./startup.sh 
+ln -s /opt/apache-tomcat-8.5.35/bin/startup.sh /usr/local/bin/tomcatup
+ln -s /opt/apache-tomcat-8.5.35/bin/shutdown.sh /usr/local/bin/tomcatdown
+tomcatup
+
+
+#checkpoint
+cd /opt/apache-tomcat-8.5.35/conf
+# update port number in the "connecter port" field in server.xml
+# restart tomcat after configuration update
